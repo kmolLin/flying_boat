0d18d6df0b2fa237f9896462493dc6f18c4f7277
qqq <@@@>
2016-03-31 21:55:48 +0900

    03310855

--- FlyBoat/app/src/main/java/com/example/user/flyboat/MainActivity.java
+++ FlyBoat/app/src/main/java/com/example/user/flyboat/MainActivity.java
@@ -6,27 +6,35 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
-import com.example.user.flyboat.Rocker;
+import com.example.user.flyboat.VerticalSeekBar;
+import com.example.user.flyboat.VerticalSeekBar_Reverse;
 
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
+    VerticalSeekBar verticalSeekBar = null;
+    TextView vsProgress;
+
+    Rocker rocker;
+    TextView status;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        final TextView status = (TextView) findViewById(R.id.status);
+        findviewid();
 
-        Rocker rocker = (Rocker) findViewById(R.id.rudder);
+        seekBarActivity();
 
         Bitmap rocker_bg = BitmapFactory.decodeResource(getResources(), R.drawable.rocker_bg1);
         Bitmap rocker_ctrl = BitmapFactory.decodeResource(getResources(), R.drawable.rocker_ctrl);
         rocker.setRockerBg(rocker_bg);
         rocker.setRockerCtrl(rocker_ctrl);
-
         rocker.setRudderListener(new Rocker.RudderListener() {
 
             @Override
@@ -34,10 +42,35 @@ public class MainActivity extends Activity {
                 if (action == Rocker.ACTION_RUDDER) {
                     //TODO:事件实现
                     //	Log.e("夹角", "angle"+angle);
-                    status.setText("角度:"+angle);
+                    status.setText("角度:" + angle);
                 }
             }
         });
     }
 
+    private void findviewid(){
+        status = (TextView) findViewById(R.id.status);
+        rocker = (Rocker) findViewById(R.id.rudder);
+        verticalSeekBar=(VerticalSeekBar)findViewById(R.id.vertical_Seekbar);
+        vsProgress=(TextView)findViewById(R.id.vertical_sb_progresstext);
+    }
+
+    private void seekBarActivity() {
+        verticalSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                vsProgress.setText(progress+"");
+            }
+        });
+    }
 }
